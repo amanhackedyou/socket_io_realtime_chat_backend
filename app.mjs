3ea015a7ffff88bfe8b70d8b355ccf1cf26620ec
Aman Programmer <ay12121230@gmail.com>
2023-01-18 03:04:50 +0530

    Debug

--- app.mjs
+++ app.mjs
@@ -10,15 +10,33 @@ io.on("connection", (socket) => {
     return socket.leave(socket.id);
   }
   users[socket.id] = socket.request.headers.username;
-  io.emit("online_users_count", io.engine.clientsCount.toString());
+  io.emit("online_user_count", io.engine.clientsCount.toString())
+  socket.broadcast.emit("users_connection_status", {
+    "count": io.engine.clientsCount.toString(),
+    "username": users[socket.id],
+    "type": "joined"
+  });
+
+  setTimeout(() => {
+    io.to(socket.id).emit("incoming", {
+      username: "Bot",
+      msg: `Hello ${users[socket.id]}, Welcome to Realtime Chat Application by iMobCode!`
+    });
+  }, 1000);
 
   socket.on("message", (data) => {
     socket.broadcast.emit("incoming", data);
   })
 
   socket.on("disconnect", async () => {
+    io.emit("users_connection_status", {
+      "username": users[socket.id],
+      "type": "left"
+    });
+
+    socket.broadcast.emit("online_user_count", io.engine.clientsCount.toString())
+
     delete users[socket.id];
-    io.emit("online_users_count", io.engine.clientsCount.toString());
   })
 });
 
